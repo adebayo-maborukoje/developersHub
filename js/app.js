05f64f72557e054dea36c60db5801301fcd90d36
andela-amaborukoje <adebayo@andela.co>
2015-01-20 08:24:30 +0100

    resolving auto merge conflicts

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 var yearbookAPP = angular.module('yearbookAPP', ['ngRoute']);
 
 
-yearbookAPP.config(['$routeProvider', function($routeProvider){  
+yearbookAPP.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){ 
+  $locationProvider.html5Mode(true);
   $routeProvider.
   when('/home', {
     templateUrl:'/partials/home.html',
     controller: 'homeCtrl'
-  }).
-  when('/form', {
-    templateUrl: 'partials/form.html',
+  })
+  .when('/repos', {
+    templateUrl: '/partials/repository.html',
+    controller: 'reposCtrl'
+  })
+  .when('/form', {
+    templateUrl: '/partials/form.html',
     controller : 'formController'
-  }).
-  when('/:username', {
-    templateUrl: 'partials/member.html',
+  })
+  // .when('/login', {
+  //   templateUrl: '/partials/login.html',
+  //   controller:'loginCtrl'
+  // })
+  .when('/:username', {
+    templateUrl: '/partials/member.html',
     controller: 'memberCtrl'
-  }).
-  otherwise({
+  })
+  .otherwise({
     redirectTo: '/home'
   });
 
--- js/controller.js
+++ js/controller.js
@@ -1,139 +1,67 @@
 var yearbookAPP = angular.module('yearbookAPP');
-var urlBase =  'https://andela-yearbook.herokuapp.com/yearBook/';
-// var urlBase = 'https://lagbusstops.herokuapp.com/api'
+var urlBase =  'https://developershubapi.herokuapp.com/yearBook/';
+
 yearbookAPP.controller('homeCtrl', function ($scope, $http){
-  // $scope.getData = function(){
+ console.log("HOME CONTROLLER");
 
-    $http.get(urlBase).success(function(data){
-       $scope.lists = data;
-        console.log($scope.lists);
-      });
-
-
-
-// scope.modalUpdate = function (size) {
-
-//    var modalInstance = $modal.open({
-//      templateUrl: 'partials/form.html',
-//      controller: function ($scope, $modalInstance) {
-       
-//        $scope.ok = function () {
-//            $modalInstance.close();
-//        };
-
-//        $scope.cancel = function () {
-//          $modalInstance.dismiss('cancel');
-//        };
-//      },
-//      size: size,
-//      resolve: {
-//        book: function () {
-//          return selected;
-//        }
-//      }
-//    });
-
-//    modalInstance.result.then(function (selectedItem) {
-//      $scope.selected = selectedItem;
-//    }, function () {
-//      $log.info('Modal dismissed at: ' + new Date());
-//    });
-//  };
 });
 
-// $scope.postData = function(){
-//     $http(
-//       {
-//       method  : 'POST',
-//       url     : 'https://lagbusstops.herokuapp.com/api',
-//       data    : $.param({name:$scope.bustop, region:$scope.region}),  
-//       headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-//     }).success(function () {
-//         console.log('Item edited');
-//     }).error(function () {
-//         console.log('Error occured');
-//       });
-//   };
+yearbookAPP.controller('reposCtrl', function ($scope, $http){
+    $http.get(urlBase).success(function(data){
+       $scope.lists = data;
+        // $localStorage.memberData = $scope.lists;
+        console.log($scope.lists);
+      });
+});
 
-// yearbookAPP.controller('formController', function ($scope, $http, fileUpload){
-yearbookAPP.controller('formController', function ($scope, $http){
-
-  $scope.submit =  function (){
-     
+yearbookAPP.controller('formController', function ($scope, $http, $location){
+ $scope.submit = function (newUser) {
       $http({
           method: 'POST',
           url: urlBase,
-          data : $.param(
-            {
-                firstname: $scope.first_name,
-                lastname: $scope.last_name,
-                // email: $scope.email,
-                username: $scope.username,
-                nickname: $scope.nickname,
-                occupation:$scope.occupation,
-                organisation:$scope.organisation,
-                marital:$scope.marital,
-                graduation: $scope.graduation,
-                birthday: $scope.birthday,
-                mobile: $scope.mobile,
-                hobbies: $scope.hobbies
-                // bio: $scope.bio
-            }),
+          data : $.param(newUser),
           headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-      }).success(function(){
 
-        // console.log(birthday + graduation + firstname);
-        // var file = $scope.fileToUpload;
-        // var uploadUrl = 'https://andela-yearbook.herokuapp.com/yearBook/uploads';
-        // fileUpload.uploadFileToUrl(file, uploadUrl);
-       console.log("got here");
+      }).success(function(){
+          console.log(newUser)
+          $scope.report="Created Successfully click the repo to view";
+          console.log("got here");
       }).error(function (err){
-        console.log("error occurred here");
-        
+        $scope.report ="Error Processing your Form";
       });
-  };
+    } 
 });
 
-yearbookAPP.controller('memberCtrl', function ($scope, $http, $routeParams){
+yearbookAPP.controller('memberCtrl', function ($scope, $http, $routeParams, $location){
   var query = $routeParams.username;
   console.log(urlBase+query);
   var url = urlBase+query;
-  $http.get(url).success(function(data){
-  $scope.member = data;
-  console.log($scope.member);
-// }).error(function (err, status){
-//   $scope.member = err;
-//   return;
- });
+  $http.get(url).success(function(data, status){
+    if (status !== 200 || data.status === 404){
+         $location.path('/home');
+        // console.log(status, data.status)
+    }else{
+        console.log(status);
+        $scope.member = data;
+        console.log($scope.member);
+     }
+  });
 });
 
-// yearbookAPP.directive('fileModel', ['$parse', function ($parse) {
-//     return {
-//         restrict: 'A',
-//         link: function(scope, element, attrs) {
-//             var model = $parse(attrs.fileModel);
-//             var modelSetter = model.assign;
-            
-//             element.bind('change', function(){
-//                 scope.$apply(function(){
-//                     modelSetter(scope, element[0].files[0]);
-//                 });
-//             });
-//         }
-//     };
-// }]);
-
-// yearbookAPP.service('fileUpload', ['$http', function ($http) {
-//     this.uploadFileToUrl = function(file, uploadUrl){
-//         var fd = new FormData();
-//         fd.append('file', file);
-//         $http.post(uploadUrl, fd, {
-//             transformRequest: angular.identity,
-//             headers: {'Content-Type': undefined}
-//         })
-//         .success(function(){
-//         })
-//         .error(function(){
-//         });
-//     }
-// }]);
+yearbookAPP.controller('loginCtrl', function($scope, $http, $routeParams, $location){
+  $scope.submit = function (){
+    $http({
+      method: 'POST',
+      url: urlBase,
+      data: $.param ({
+        username: $scope.username,
+        password: $scope.password
+        }),
+      header: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).success(function(user){
+      $location.url('/edit')
+    }).error(function(){
+      $scope.report = "USERNAME OR PASSWORD INCORRECT";
+    });
+  }
+});
